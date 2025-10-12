08d8213ec4a36a0eb8fc5b8472873983c269883c
Nawin Kumar <nawinkumar@Nawins-MacBook-Air.local>
2025-10-12 15:04:16 +0530

    NK

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Analytics />
         <Navbar />
         <main>{children}</main>
         <Footer />
